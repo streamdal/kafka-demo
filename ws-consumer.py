6b4b939c37f5adb33b750cf2a5a6914ec539648b
Fritz Stauffacher <fritzstauff@gmail.com>
2021-08-13 14:57:16 -0400

    Second commit

--- ws-consumer.py
+++ ws-consumer.py
@@ -5,32 +5,45 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 
-def consume(topic,kafka_url):
+def consume(topic,kafka_url,bucket,org):
   consumer = kafka.KafkaConsumer(topic, bootstrap_servers=kafka_url, auto_offset_reset='earliest', enable_auto_commit=True, group_id='batch-consumer')
  
-  ## Influx settings
-
-  token = "bNCydUmEkAkxtW-dmtfc6WRJnRXSRiZapkAqlii3xQtQBrULO1bwPr7OlgkCpmJdDcE1p38NcjB0fRiDMSiX8Q=="
-  org = "batch"
-  bucket = "rsvp"
-  client = InfluxDBClient(url="http://127.0.0.1:8086", token=token)
-
   for msg in consumer:
     message=json.loads(msg.value.decode('utf-8'))
     if "venue" in message and "group_state" in message["group"]:
       if message["group"]["group_state"] != "CA":
-        print("group_name: %s response: %s group_state: %s group_id: %d" % (message["group"]["group_name"], message["response"],message["group"]["group_state"], message["group"]["group_id"]))
         if message["response"] == "yes":
           response=1
         else:
           response=0
         data = "event,group_id=%d,event_id='%s',group_state='%s' response=%d"  % (message["group"]["group_id"], message["event"]["event_id"], message["group"]["group_state"], response)
-        #data='event,group_name="Newgroup\ \Test",group_state="on" group_id=1,response=0' 
         write_api = client.write_api(write_options=SYNCHRONOUS)
         write_api.write(bucket, org, data)
-      
+
+       
+
+def influx_connect(token, url):
+  client = InfluxDBClient(url=url, token=token)
+  return client
+
+#def influx_write(client, data, bucket, org):
+#  ## Influx settings
+#  write_api = client.write_api(write_options=SYNCHRONOUS)
+#  write_api.write(bucket, org, data)
+
+#Influx
+url="http://127.0.0.1:8086"
+token="bNCydUmEkAkxtW-dmtfc6WRJnRXSRiZapkAqlii3xQtQBrULO1bwPr7OlgkCpmJdDcE1p38NcjB0fRiDMSiX8Q=="
+
+#Kafka
 topic='rsvps'
 kafka_url=['127.0.0.1:9092']
+org = "batch"
+bucket = "rsvp"
 
-consume(topic, kafka_url)
+
+#functions
+client=influx_connect(token, url)
+data=consume(topic, kafka_url, bucket, org)
+#influx_write(client, data, bucket, org)
 #write_topics(kafka_url, topic)
